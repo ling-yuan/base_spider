f388bf8c5b353ee2ac05e322073dec0a5d6e76d6
baishi <572268633@qq.com>
2024-07-30 14:10:08 +0800

    add: mysql pipeline

---
+++ universal_spider/Info/mysql.py
@@ -0,0 +1,9 @@
+MYSQL_HOST = "localhost"  # 主机地址
+MYSQL_PORT = 3306  # 端口号
+MYSQL_USER = "root"  # 用户名
+MYSQL_PASSWORD = "root"  # 密码
+MYSQL_DATABASE = "base_spider_db"  # 数据库名称
+MYSQL_TABLE = "base_spider_table"  # 表名称
+MYSQL_CHARSET = "utf8mb4"  # 字符集
+
+MYSQL_CLEAR_DB = True  # 是否清空数据库
